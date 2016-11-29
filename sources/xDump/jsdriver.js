b5f1d3e075bb4e0277e53f084bd80d2a3148c032
Petr Kurapov <petr.kurapov@gmail.com>
2016-11-29 21:27:18 +0300

    [JS] Updating JS skeleton

--- sources/xDump/jsdriver.js
+++ sources/xDump/jsdriver.js
@@ -1,21 +1,54 @@
 
+function LineStream() {}
+
+LineStream.prototype = {
+    getLine : function() {},
+    getLines : function() {},
+    data : ''
+}
+
+// Global?
+dataContainer = new LineStream();
+
 function ViewConfig(cfg) {
     if(typeof(cfg) === 'undefined') {this.config = {}; return;}
     this.config = cfg;
 }
 
-function Config() {}
-function Executer() {}
+function Executor() {}
 
 
-Executer.prototype =  {
-    config : {},
-    exec : function (options) {}
+Executor.prototype =  {
+    config : {
+        common : ['$(PATH)', '$(EXE_NAME=objdump)'],
+        sections : {
+            globHeader : '-h $(OPT=) $(INP_FILE)',
+            //...
+        }
+    },
+    exec : function (options) {
+        // Run objdump here
+        dataContainer.data = 'objdump >';
+
+        return ;
+    },
+
 }
 
-ViewConfig.prototype = {
-    viewTranslator : function () {
 
+ViewConfig.prototype = {
+    viewTranslatorConfig : {
+        // From config file?
+        header : '<html>',
+        footer : '</html>'
+    },
+    viewTranslator : {
+        getHeader   : function() {return '<html><body>'/*this.viewTranslatorConfig.header; ???*/;},
+        getFooter   : function() {return '</body></html>';},
+        getBody     : function() {
+            var body = 'body';
+            return body;
+        }
     },
     execDriver : function () {}
 }
