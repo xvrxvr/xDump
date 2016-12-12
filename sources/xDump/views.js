a564301f844b9fbc7698cf927ed1a3383f352246
Petr Kurapov <petr.kurapov@gmail.com>
2016-12-12 22:35:44 +0300

    Updating files structure

--- sources/xDump/views.js
+++ sources/xDump/views.js
@@ -1,4 +1,21 @@
-
-function func() {
+function SectionsViewTranslator(data, config) {
+    this.data = data.getLines();
 
 }
+
+SectionsViewTranslator.prototype = {
+    getHeader : function () {
+        return '<!DOCTYPE html>' +
+                '<html>' +
+                '<body>' +
+                '<table>';
+    },
+    getFooter : function () {
+        return '</table>' +
+                '</body>' +
+                '</html>';
+    },
+    getBody   : function () {}
+}
+
+/*--------------------------------------------------------------------------*/
